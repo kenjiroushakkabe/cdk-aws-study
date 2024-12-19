94f5cf4ec57e1565ccc3e7256eca132cebf98533
kenjiroushakkabe <kenjiro.shakkabe.zn9@nri-st.co.jp>
2024-12-19 03:19:01 +0000

    Lambdaのエラーをメールで検知する機能を追加する。 Fixes #6

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import path from 'path';
-import { App, CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { App, CfnOutput, Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { FilterPattern, RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
 import { HttpApi, HttpMethod } from 'aws-cdk-lib/aws-apigatewayv2';
 import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
+import { ComparisonOperator, Metric, TreatMissingData } from 'aws-cdk-lib/aws-cloudwatch';
+import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
 
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-
+    const alertTopic = new Topic(this, 'alert-topic', {
+      fifo: false,
+    });
+    alertTopic.addSubscription(new EmailSubscription('kenjiro.shakkabe.zn9@nri-st.co.jp'));
 
     const func = new NodejsFunction(this, 'hello-world', {
       entry: path.join(__dirname, 'lambda/hello-world.ts'),
@@ -20,6 +27,30 @@ export class MyStack extends Stack {
       runtime: Runtime.NODEJS_LATEST,
       logRetention: RetentionDays.THREE_MONTHS,
     });
+
+    const metricNamespace = 'LogMetrics';
+    const metricName = 'Error';
+    func.logGroup.addMetricFilter('erro-metric-filter', {
+      metricNamespace,
+      metricName,
+      filterPattern: FilterPattern.literal('Error'),
+    });
+
+    const metric = new Metric({
+      namespace: metricNamespace,
+      metricName,
+      period: Duration.minutes(1),
+      statistic: 'Sum',
+    });
+
+    metric.createAlarm(this, 'error-alarm',{
+      alarmName: 'error-alarm',
+      evaluationPeriods: 1,
+      threshold: 1,
+      treatMissingData: TreatMissingData.NOT_BREACHING,
+      comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+      actionsEnabled: true,
+    }).addAlarmAction(new SnsAction(alertTopic));
 
     const api = new HttpApi(this, 'Api');
     api.addRoutes({
